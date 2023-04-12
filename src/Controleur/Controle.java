d0d50916effd2c49f4662d8bc5701fd84257bb62
Nbrice237 <makebelbrice@gmail.com>
2023-04-12 14:28:05 +0200

    Utilisation des outils de connexion pour créer le serveur, s'y connecter et ouvrir les fenêtres.

--- src/Controleur/Controle.java
+++ src/Controleur/Controle.java
@@ -4,15 +4,24 @@
 package Controleur;
 
 import Vue.EntreeJeu;
+import Vue.Arene;
+import Vue.ChoixJoueur;
+import outils.connexion.ServeurSocket;
+import outils.connexion.AsyncResponse;
+import outils.connexion.ClientSocket;
+import outils.connexion.Connection;
 
 /**
  * Contr�leur et point d'entr�e de l'applicaton 
  * @author makebel noel brice
  *
  */
-public class Controle {
+public class Controle implements AsyncResponse{
 	
 	private EntreeJeu frmEntreeJeu ;
+	private Arene frmArene;
+	private ChoixJoueur frmChoixJoueur;
+	private String typeJeu;
 
 	/**
 	 *  Methode de demarrage
@@ -27,8 +36,45 @@ public class Controle {
 	 * Constructeur
 	 */
 	private Controle() {
-		this.frmEntreeJeu = new EntreeJeu() ;
+		this.frmEntreeJeu = new EntreeJeu(this) ;
 		this.frmEntreeJeu.setVisible(true);
+	}
+	
+	/**
+	 * 
+	 */
+	public void evenementEntreeJeu(String info) {
+		if(info.equals("serveur")) {
+			this.typeJeu = "serveur";
+			new ServeurSocket(this, 6666);
+			this.frmEntreeJeu.dispose();
+			this.frmArene = new Arene();
+			this.frmArene.setVisible(true);
+			
+		}
+		else {
+			this.typeJeu = "client";
+			new ClientSocket(this, info, 6666);
+		}
+	}
+
+	@Override
+	public void reception(Connection connection, String ordre, Object info) {
+		switch(ordre) {
+		case "connexion":
+			if(this.typeJeu.equals("client")) {
+				this.frmEntreeJeu.dispose();
+				this.frmArene = new Arene();
+				this.frmChoixJoueur = new ChoixJoueur();
+				this.frmChoixJoueur.setVisible(true);
+			}
+			break;
+		case "réception":
+			break;
+		case "déconnexion":
+			break;
+		}
+		
 	}
 
 }
--- src/Vue/EntreeJeu.java
+++ src/Vue/EntreeJeu.java
@@ -1,5 +1,7 @@
 package Vue;
+
 import java.awt.EventQueue;
+import Controleur.Controle;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -9,65 +11,78 @@ import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
+import java.util.ResourceBundle.Control;
 import java.awt.event.ActionEvent;
 
 public class EntreeJeu extends JFrame {
 
 	private JPanel contentPane;
+	/**
+	 * saisie de l'ip adresse serveur
+	 */
 	private JTextField txtServer;
-  /**
-   *  clic sur le bouton ferme l'application
-   */
-	public void BtnExit() {
+	
+	/**
+	 * propriété controle de type Controle
+	 */
+	private Controle controle;
+
+	/**
+	 * clic sur le bouton ferme l'application
+	 */
+	private void BtnExit() {
 		System.exit(0);
 	}
-	
+
 	/**
 	 * Le clic sur le bouton ouvre la fenêtre "Arene".
 	 */
-	public void BtnStart() {
-		Arene arene = new Arene();
+	private void BtnStart() {
+		/**Arene arene = new Arene();
 		arene.setVisible(true);
-		this.dispose();
+		this.dispose();*/
+		
+		this.controle.evenementEntreeJeu("serveur");
 	}
-	
+
 	/**
 	 * Le clic sur le bouton ouvre la fenêtre "ChoixJoueur".
 	 */
-	public void BtnConnect() {
-		ChoixJoueur choix = new ChoixJoueur();
+	private void BtnConnect() {
+		/**ChoixJoueur choix = new ChoixJoueur();
 		choix.setVisible(true);
-		this.dispose();
+		this.dispose();*/
+		this.controle.evenementEntreeJeu(this.txtServer.getText());
 	}
 
 	/**
 	 * Create the frame.
 	 */
-	public EntreeJeu() {
+	public EntreeJeu(Controle controle) {
 		setResizable(false);
 		setTitle("Urban Marginal");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 500, 280);
 		contentPane = new JPanel();
-		//contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		// contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblStart = new JLabel("Start a server :");
 		lblStart.setFont(new Font("Arial Black", Font.BOLD, 18));
 		lblStart.setBounds(24, 31, 186, 36);
 		contentPane.add(lblStart);
-		
+
 		JLabel lblConnect = new JLabel("Connect an existing server :");
 		lblConnect.setFont(new Font("Arial Black", Font.BOLD, 18));
 		lblConnect.setBounds(23, 84, 299, 44);
 		contentPane.add(lblConnect);
-		
+
 		JLabel lblIPserver = new JLabel("IP server :");
 		lblIPserver.setFont(new Font("Arial Black", Font.BOLD, 18));
 		lblIPserver.setBounds(24, 149, 147, 36);
 		contentPane.add(lblIPserver);
-		
+
 		JButton btnStart = new JButton("Start\r\n");
 		btnStart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -77,7 +92,7 @@ public class EntreeJeu extends JFrame {
 		btnStart.setFont(new Font("Arial Black", Font.BOLD, 18));
 		btnStart.setBounds(323, 34, 147, 36);
 		contentPane.add(btnStart);
-		
+
 		JButton btnExit = new JButton("Exit\r\n");
 		btnExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -87,14 +102,14 @@ public class EntreeJeu extends JFrame {
 		btnExit.setFont(new Font("Arial Black", Font.BOLD, 18));
 		btnExit.setBounds(323, 205, 147, 34);
 		contentPane.add(btnExit);
-		
+
 		txtServer = new JTextField();
 		txtServer.setFont(new Font("Arial Black", Font.BOLD, 18));
 		txtServer.setText("127.0.0.1");
 		txtServer.setBounds(141, 144, 168, 36);
 		contentPane.add(txtServer);
 		txtServer.setColumns(10);
-		
+
 		JButton btnConnect = new JButton("Connect");
 		btnConnect.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -104,5 +119,10 @@ public class EntreeJeu extends JFrame {
 		btnConnect.setFont(new Font("Arial Black", Font.BOLD, 18));
 		btnConnect.setBounds(323, 141, 147, 36);
 		contentPane.add(btnConnect);
+		
+		/**
+		 * valorisons la propriété
+		 */
+		this.controle = controle;
 	}
 }
